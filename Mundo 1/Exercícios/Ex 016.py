e4ff5c1729cc9db4532e7c13e574f300e96c7985
Igor Moreira <igor.morvale@gmail.com>
2022-08-27 19:09:24 -0300

    Add algumas informações: comentários e título do exercício

--- Mundo 1/Exercícios/Ex 016.py
+++ Mundo 1/Exercícios/Ex 016.py
@@ -1,11 +1,23 @@
+# Exrecício 016 #
+#
+# Crire um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+
 print('\033[1;33m-=' *19)
 print('\033[1;34m-=-=-=-=-=- Exercício 016 -=-=-=-=-=-')
 print('\033[1;33m-=\033[m' *19)
 
+# Importando a função math para utilizar o truncamento
 import math
 
+# Definição da variável inserida pelo usuário
 num = float(input('Digite um número: '))
 
+# Identificando a porção inteira do número lido pela variável 'num'
 inteiro = math.trunc(num)
 
-print('O número {} tem a parte inteira {}.'.format(num, inteiro))
+# Imprimindo o resultado na tela
+print('Resultado 1: O número {} tem a parte inteira {}.'.format(num, inteiro))
+
+# Uma outra forma de fazer sem utilizar a função 'trunc', é utilizar a segunte linha de código:
+print('Resultado 2: O número {} tem a parte inteira {}.'.format(num, int(num)))
