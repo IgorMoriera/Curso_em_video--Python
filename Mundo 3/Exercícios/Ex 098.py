2b021605599cb083b1a4557eb12b620150e28f31
Igor Moreira <igor.morvale@gmail.com>
2024-04-22 21:03:33 -0300

    Atualizando os códigos
    Adicionando o enunciado e corrigindo alguns bugs.

--- Mundo 3/Exercícios/Ex 098.py
+++ Mundo 3/Exercícios/Ex 098.py
@@ -1,12 +1,26 @@
+# Exercício 098 #
+#
+# Faca um programa que tenha uma função chamada contador(), que receba três paraâmetros: início, fim e passo e realize
+#  a contagem.
+#
+# Seu programa tem que realizar três contagens atraveés da função criada:
+#
+#  a) De 1 até 10, de 1 em 1;
+#  b) De 10 até 0, de 2 em 2;
+#  c) Uma contagem personalizada.
+
 print('\033[1;33m-=' * 19)
 print('\033[1;34m-=-=-=-=-=- Exercício 098 -=-=-=-=-=-')
 print('\033[1;33m-=\033[m' * 19)
 
 from time import sleep
 
+
 def contador(i, f, p):
+
+    # Tratando as condições do passo
     if p < 0:
-        p *= 1
+        p = abs(p)
 
     if p == 0:
         p = 1
@@ -14,21 +28,23 @@ def contador(i, f, p):
     print('-=' * 20)
     print(f'Contagem de {i} até {f} de {p} em {p}')
 
+    # Realizando a contagem de forma crescente
     if i < f:
         cont = i
         while cont <= f:
             print(f'{cont} ', end='')
-            sleep(0.5)
+            sleep(0.3)
             cont += p
-        print('FIM!')
+        print(' -- FIM!')
 
+    # Realizando a contagem de forma regressiva
     else:
         cont = i
         while cont >= f:
             print(f'{cont} ', end='')
-            sleep(0.5)
+            sleep(0.3)
             cont -= p
-        print('FIM!')
+        print('-- FIM!')
 
 
 # Programa principal
