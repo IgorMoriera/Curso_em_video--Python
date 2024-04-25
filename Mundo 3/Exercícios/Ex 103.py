ecb7bd86a74fa9ea80583aa9a2610f7f783e9f06
Igor Moreira <igor.morvale@gmail.com>
2024-04-25 19:37:25 -0300

    Atualizando os códigos
    Adicionando o enunciado e corrigindo alguns bugs.

--- Mundo 3/Exercícios/Ex 103.py
+++ Mundo 3/Exercícios/Ex 103.py
@@ -9,3 +9,19 @@ print('\033[1;33m-=' * 19)
 print('\033[1;34m-=-=-=-=-=- Exercício 103 -=-=-=-=-=-')
 print('\033[1;33m-=\033[m' * 19)
 
+nome_jogador = "<desconhecido>"
+
+def ficha (nome_jogador="<desconhecido>", gols=0):
+
+    if nome_jogador != "<desconhecido>":
+        nome_jogador = str(input('Nome do jogador: '))
+        gols = str(input('Quantos gols ele marcou: '))
+
+    else:
+        nome_jogador == "<desconhecido>"
+
+    return print(f'O jogador {nome_jogador} fez {gols} gol(s) no campeonato.')
+
+
+ficha()
+
