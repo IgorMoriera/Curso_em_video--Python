75aab8b0bf1da1b7da09c47ac92e8e2f6e78bfc2
Igor Moreira <igor.morvale@gmail.com>
2024-05-02 22:06:46 -0300

    Subindo exercício 114
    Além disso, corrigindo alguns arquivos do repositório.

--- Mundo 3/Exercícios/Ex 114.py
+++ Mundo 3/Exercícios/Ex 114.py
@@ -8,3 +8,14 @@ print('\033[1;33m-=' * 19)
 print('\033[1;34m-=-=-=-=-=- Exercício 114 -=-=-=-=-=-')
 print('\033[1;33m-=\033[m' * 19)
 
+import requests
+
+try:
+    resp = requests.get('https://www.pudim.com.br')
+    resp.raise_for_status()
+
+except requests.exceptions.RequestException:
+    print("\033[1;31mO site Pudim não está acessível no momento.\033[m")
+
+else:
+    print("\033[1;32mConexão estabelecida com sucesso!\033[m")
